f094bcac5a37cb6f5829fa7e34a8f8b7e8f64e60
DontWorryAboutIt <brazea435@morrisville.edu>
2020-03-31 18:15:40 -0400

    Moving Using transform.Translate

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class Attacker : MonoBehaviour
+{
+    [Range(0f, 5f)]
+    [SerializeField] float walkSpeed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.right * walkSpeed *Time.deltaTime);
+    }
+}
