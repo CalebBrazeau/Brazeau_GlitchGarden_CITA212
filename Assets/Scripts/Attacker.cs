7651edcf8a48f6f842bf3f761c83fb4a760fd4a3
DontWorryAboutIt <brazea435@morrisville.edu>
2020-03-31 19:30:19 -0400

    Adding Animation Events

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -6,8 +6,9 @@ using UnityEngine;
 public class Attacker : MonoBehaviour
 {
     [Range(0f, 5f)]
-    [SerializeField] float walkSpeed = 5f;
+    float currentSpeed = 1f;
     private SpriteRenderer mySpriteRenderer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +19,12 @@ public class Attacker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector2.left * walkSpeed *Time.deltaTime);
+        transform.Translate(Vector2.left * currentSpeed * Time.deltaTime);
     }
 
+    public void SetMovementSpeed(float speed)
+    {
+        currentSpeed = speed;
+
+    }
 }
