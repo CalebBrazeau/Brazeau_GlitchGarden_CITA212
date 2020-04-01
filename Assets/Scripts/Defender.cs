519b34cb74560c9cca249565c7013696a3d312a1
DontWorryAboutIt <brazea435@morrisville.edu>
2020-03-31 20:08:19 -0400

    Instantiate From Gun Position

---
+++ Assets/Scripts/Defender.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Defender : MonoBehaviour
+{
+    [SerializeField] GameObject Projectile;
+    [SerializeField] GameObject TheStrap;
+
+    public void Fire()
+    {
+        Instantiate(Projectile, TheStrap.transform.position, transform.rotation);
+    }
+
+
+}
