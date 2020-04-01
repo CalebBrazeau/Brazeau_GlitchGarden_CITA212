7d6c8b5babc7ce5b5d16adf2243b54cb7a75d0ee
DontWorryAboutIt <brazea435@morrisville.edu>
2020-03-31 20:40:49 -0400

    Implemening Health & Damage

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,13 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
     [SerializeField] float speed = 1f;
+    [SerializeField] float damage = 20f;
 
     void Update()
     {
         transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        var health = otherCollider.GetComponent<Health>();
+        health.DamageMeOwO(damage);
     }
 }
