74524511a6585a497716daed6067648395503b25
DontWorryAboutIt <brazea435@morrisville.edu>
2020-04-02 15:44:28 -0400

    Attacker In My Lane

--- Assets/Scripts/Defender.cs
+++ Assets/Scripts/Defender.cs
@@ -1,11 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class Defender : MonoBehaviour
 {
     [SerializeField] GameObject Projectile;
     [SerializeField] GameObject TheStrap;
+    AttackerSpawner myLaneSpawner;
+
+    void Start()
+    {
+        SetLaneSpawner();
+    }
+
+    void Update()
+    {
+        if (IsAttackerInLane())
+        {
+            //Debug.Log("Shoot Pew Pew");
+        }
+        else
+        {
+            //Debug.Log("Wait");
+        }
+    }
+
+    private void SetLaneSpawner()
+    {
+        AttackerSpawner[] spawners = FindObjectsOfType<AttackerSpawner>();
+
+        foreach (AttackerSpawner spawner in spawners)
+        {
+            bool IsCloseEnough = (Mathf.Abs(spawner.transform.position.y - transform.position.y) <= Mathf.Epsilon);
+
+            if (IsCloseEnough)
+            {
+                myLaneSpawner = spawner;
+            }
+        }
+    }
+
+    private bool IsAttackerInLane()
+    {
+        if (myLaneSpawner.transform.childCount <= 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
 
     public void Fire()
     {
