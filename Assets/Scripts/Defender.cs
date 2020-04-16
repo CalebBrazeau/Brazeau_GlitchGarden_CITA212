ca1f3767fe4f21de3ba3bac47f5e0173c8c20965
DontWorryAboutIt <brazea435@morrisville.edu>
2020-04-16 13:20:31 -0400

    Attacker In My Lane

--- Assets/Scripts/Defender.cs
+++ Assets/Scripts/Defender.cs
@@ -7,6 +7,59 @@ public class Defender : MonoBehaviour
     // Sets gameobject for projectile and gun
     [SerializeField] GameObject Projectile;
     [SerializeField] GameObject TheStrap;
+    AttackerSpawner myLaneSpawner;
+    //Animator myAnimator;
+
+    void Start()
+    {
+        //myAnimator = GetComponent<Animator>();
+
+        SetLaneSpawner();
+
+    }
+    void Update()
+    {
+        // IF there is an attacker in lane
+        if(IsAttackerInLane())
+        {
+            print("Shoot Pew Pew");
+
+        }
+        // If there isnt an attacker in lane
+        else
+        {
+            print("Just Chill Dude");
+
+        }
+    }
+
+    private void SetLaneSpawner()
+    {
+        AttackerSpawner[] spawners = FindObjectsOfType<AttackerSpawner>();
+        // for each attacker in spawner
+        foreach(AttackerSpawner spawner in spawners)
+        {
+            // if the spawner position and the defender postition = 0ish 
+            bool boolIsCloseEnough = (Mathf.Abs(spawner.transform.position.y - transform.position.y) <= Mathf.Epsilon);
+            // if the bool is true
+            if (boolIsCloseEnough)
+            {
+                myLaneSpawner = spawner;
+            }
+        }
+    }
+
+    private bool IsAttackerInLane()
+    {
+        if (myLaneSpawner.transform.childCount <= 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
 
     public void Fire()
     {
