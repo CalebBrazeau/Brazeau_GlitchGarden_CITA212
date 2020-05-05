7d2fd866028ae70b361729be6359db13a67f4ef8
DontWorryAboutIt <brazea435@morrisville.edu>
2020-05-05 15:28:24 -0400

    Set & Save Music Volume

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Tells script to not destroy itself in the next load sequence
+        DontDestroyOnLoad(this);
+        // gets the audio source component
+        audioSource = GetComponent<AudioSource>();
+        // Sets audio sources volume to whatever the master volume value is
+        audioSource.volume = PlayerPrefsController.GetMasterVolume();
+    }
+
+    public void SetVolume(float fltVolume)
+    {
+        audioSource.volume = fltVolume;
+    }
+}
