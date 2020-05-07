427b4646adedeee194f9cf65e0590bda1c58a5c8
DontWorryAboutIt <brazea435@morrisville.edu>
2020-05-07 13:51:38 -0400

    Add Star Cost To Button

--- Assets/Scripts/DefenderButton.cs
+++ Assets/Scripts/DefenderButton.cs
@@ -1,10 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DefenderButton : MonoBehaviour
 {
     [SerializeField] Defender defenderPrefab;
+
+    void Start()
+    {
+        LabelButtonWithCost();
+    }
+
+    private void LabelButtonWithCost()
+    {
+        // gets text component on children
+        Text costText = GetComponentInChildren<Text>();
+        // if there is no costText
+        if (!costText)
+        {
+            // prints the error to the console
+            print(name + "Has no cost text... add some");
+        }
+        else
+        {
+            // sets the costText to whatever is set in the defender prefab as their cost
+            costText.text = defenderPrefab.GetStarCost().ToString();
+        }
+    }
     private void OnMouseDown()
     {
         // create a temporary variable to store our DefenderButtons
